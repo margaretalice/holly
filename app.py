d91ef0ccae53fc842ffcb9bb64f5ecc4d2ef04cc
margaretalice <margaretwoods1982@gmail.com>
2020-09-05 09:07:26 +0000

    . updated

--- app.py
+++ app.py
@@ -13,6 +13,30 @@ app = Flask(__name__)
 @app.route('/index.html')
 def index():
     return render_template("index.html") 
+
+@app.route('/show_login')  # For the login Button
+def show_login():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'user_username': request.form['user_username']})  #Check if user info is in the system
+
+    if login_user:
+        if bcrypt.hashpw(request.form['user_password'].encode('utf-8'), login_user['user_password']) == login_user['user_password']:
+            session['user_username'] = request.form['user_username']
+            return redirect(url_for('index'))
+    else:
+        invalid_user = 'Invalid username/password combination'
+    return render_template('login.html', message=invalid_user)  # Error message if user info isn't in the system. 
+
+
+@app.route('/show_register')  # For the register button
+def show_register():
+    return render_template('register.html')
+
                          
                      
 if __name__ == '__main__':
